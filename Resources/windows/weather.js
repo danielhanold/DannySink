e0d7e64498c391694ac0743704920193d0b44b96
Daniel Hanold <danny@danielhanold.com>
2012-02-10 18:09:45 -0500

    Add images to weather table

--- Resources/windows/weather.js
+++ Resources/windows/weather.js
@@ -10,27 +10,40 @@ W.Weather = function() {
   // Create a function that fills an array with data for a table.
   var tableData = new Array;
   (function() {
-    var query = 'SELECT * FROM cities ORDER BY city ASC';
+    var query = 'SELECT c.city AS city, c.temp AS temp, cc.condition AS condition, cc.icon AS icon FROM cities c INNER JOIN conditionCodes cc ON cc.conditionID = c.conditionID ORDER BY city ASC';
     var db = openDB();
     var data = db.execute(query);
     while (data.isValidRow()) {
       var city = data.fieldByName('city');
       var temp = data.fieldByName('temp');
+      var icon = data.fieldByName('icon');
       var row = Ti.UI.createTableViewRow({
         selectionStyle:Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
       });
-      var labelCity = Ti.UI.createLabel({
+      var viewIcon = Ti.UI.createImageView({
         left:10,
+        image:'http://www.worldweather.org/img_cartoon/' + icon,
+        backgroundColor:'blue',
+        width:35,
+        height:35
+      });
+      Ti.API.log(JSON.stringify(viewIcon));
+      var labelCity = Ti.UI.createLabel({
+        left:60,
         text:city,
         width:'auto',
         height:'auto',
-        font:{fontWeight:'bold'}
+        font:{
+          fontWeight:'bold',
+          fontSize:14
+        }
       });
       var labelTemp = Ti.UI.createLabel({
         right:10,
         text:temp + '\u00b0C',
         width:'auto'
       });
+      row.add(viewIcon);
       row.add(labelCity);
       row.add(labelTemp);
       tableData.push(row);
