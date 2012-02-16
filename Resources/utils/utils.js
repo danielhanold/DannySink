e3f82b199c2f1e287b3d7892584c0836b894316e
Daniel Hanold <danny@danielhanold.com>
2012-02-16 14:30:50 -0500

    Add caching mechanism for weather icons.

--- Resources/utils/utils.js
+++ Resources/utils/utils.js
@@ -13,13 +13,29 @@ var Utils = {};
     // Determine if this icon is already downloaded.
     var appDir = Ti.Filesystem.applicationDataDirectory;
     var file = Ti.Filesystem.getFile(appDir, 'weathericons/' + iconName);
-    // If the file does not already exists, download and store it.
+    
+    // If the file already exists, display the file stored on the local filesystem.
     if (file.exists()) {
       Ti.API.log('Icon ' + iconName + ' exists on device filesystem. Use local file.');
       return appDir + 'weathericons/' + iconName;    
     }
+    // Otherwise, cache the image in the local filesystem, but still
+    // return the path right away, as caching takes a few seconds.
     else {
       Ti.API.log('Icon ' + iconName + ' does not exists on device. Use external URL.');
+      var imageView = Ti.UI.createImageView({
+        image:'http://www.worldweather.org/img_cartoon/' + iconName
+      });
+      // Wait a few seconds because it'll take a little bit to download the image.
+      setTimeout(function() {
+        // Valid images will be 35 pixels wide. Only store those images
+        // in the local filesystem.
+        var imageBlob = imageView.toImage();
+        Ti.API.log('Width of this icon is ' + imageBlob.width);
+        var file = Ti.Filesystem.getFile(appDir, 'weathericons/' + iconName);
+        file.write(imageBlob);
+        Ti.API.log('Successfully created this weathericon file: ' + iconName);
+      }, 5000);
       return 'http://www.worldweather.org/img_cartoon/' + iconName;
     }
   }
