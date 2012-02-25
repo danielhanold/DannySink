234b47880351bb5dcf9b563070dc02648ee27f82
Daniel Hanold <danny@danielhanold.com>
2012-02-25 17:39:35 -0500

    Add audio example for streaming sound from web.

--- Resources/windows/sound_button.js
+++ Resources/windows/sound_button.js
@@ -11,11 +11,30 @@ W.SoundButton = function() {
     height:50
   });
   
+  var buttonStream = Ti.UI.createButton({
+    title:'Stream Sound from web',
+    top:20,
+    width:200,
+    height:50
+  });
+  
+  var buttonStreamControls = Ti.UI.createButton({
+    title:'Pause Streaming Sound',
+    top:10,
+    width:250,
+    height:40,
+  });
+  
   win.add(button);
+  win.add(buttonStream);
 
   var sound = Ti.Media.createSound({
     url:Ti.Filesystem.resourcesDirectory + 'sounds/42899__freqman__canon-dos-d30-no-focus.wav',
     preload:true
+  });
+  
+  var soundStream = Ti.Media.createAudioPlayer({
+    url:'http://www.freesound.org/data/previews/2/2686_5150-lq.mp3'
   });
   
   /**
@@ -24,6 +43,28 @@ W.SoundButton = function() {
   button.addEventListener('click', function(e) {
      Ti.API.info('Sound played');
     sound.play();
+  });
+  
+  buttonStream.addEventListener('click', function(e) {
+    Ti.API.info('Start streaming sound');
+    soundStream.start();
+    // Add the stream control button.
+    win.add(buttonStreamControls);
+  });
+  
+  buttonStreamControls.addEventListener('click', function(e) {
+    // If the audio player is playing, stop it and 
+    // change the text of the button to "resume audio".
+    if (soundStream.playing) {
+      soundStream.pause();
+      this.title = "Resume Audio Stream";
+      Ti.API.info('Audio stream paused');
+    }
+    else if (soundStream.paused) {
+      soundStream.start();
+      this.title = "Pause Audio Stream";
+      Ti.API.info('Audio stream resumed');
+    }
   });
   
   return win;
